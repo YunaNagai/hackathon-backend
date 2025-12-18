17c53eaf9939871ff258defcc6f9660a8c26a401
YunaNagai <purple2yn@icloud.com>
2025-12-18 19:53:48 +0900

    update backend

--- controller/user_register_controller.go
+++ controller/user_register_controller.go
@@ -3,15 +3,20 @@ package controller
 import (
 	"database/sql"
 	"hackathon-backend/usecase"
+	"log"
 	"net/http"
 )
 
 func RegisterUserHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("RegisterUserHandler called")
+
 		if r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
+		log.Println("Calling usecase.RegisterUser")
+
 		usecase.RegisterUser(db, w, r)
 	}
 }
