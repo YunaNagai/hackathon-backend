86f3adb6def3458b1bcf7e815e8a12ed573d10e6
YunaNagai <purple2yn@icloud.com>
2025-12-19 05:44:36 +0900

    update back

--- controller/user_register_controller.go
+++ controller/user_register_controller.go
@@ -2,21 +2,32 @@ package controller
 
 import (
 	"database/sql"
-	"hackathon-backend/usecase"
-	"log"
+	"encoding/json"
 	"net/http"
+
+	"hackathon-backend/model"
+	"hackathon-backend/usecase"
 )
 
 func RegisterUserHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println("RegisterUserHandler called")
-
-		if r.Method != http.MethodPost {
-			w.WriteHeader(http.StatusBadRequest)
+		var user model.User
+		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		log.Println("Calling usecase.RegisterUser")
 
-		usecase.RegisterUser(db, w, r)
+		if user.Age < 20 || user.Age > 80 {
+			http.Error(w, "invalid age", http.StatusBadRequest)
+			return
+		}
+
+		if err := usecase.RegisterUser(db, user); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(user)
 	}
 }
--- usecase/user_register_usecase.go
+++ usecase/user_register_usecase.go
@@ -2,31 +2,10 @@ package usecase
 
 import (
 	"database/sql"
-	"encoding/json"
 	"hackathon-backend/dao"
 	"hackathon-backend/model"
-	"log"
-	"net/http"
 )
 
-func RegisterUser(db *sql.DB, w http.ResponseWriter, r *http.Request) {
-	log.Println("usecase.RegisterUser called")
-
-	var req model.User
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	if req.Age < 20 || req.Age > 80 {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if err := dao.InsertUser(db, req); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"id": req.ID})
+func RegisterUser(db *sql.DB, user model.User) error {
+	return dao.InsertUser(db, user)
 }
