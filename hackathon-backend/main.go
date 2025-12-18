289bfab96f04fe6411742fddbdd021f15348cd00
YunaNagai <purple2yn@icloud.com>
2025-12-18 13:25:50 +0900

    update backend

--- hackathon-backend/main.go
+++ hackathon-backend/main.go
@@ -1,41 +1,24 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-
 	"hackathon-backend/controller"
 	"hackathon-backend/db"
+	"hackathon-backend/middleware"
+	"log"
+	"net/http"
 )
 
 func main() {
 	database := db.Connect()
-	// ② /userでリクエストされたらnameパラメーターと一致する名前を持つレコードをJSON形式で返す
-	http.HandleFunc("/user", controller.RegisterUserHandler(database))
-	http.HandleFunc("/products", controller.ProductsHandler(database))
+	defer database.Close()
+	router := http.NewServeMux()
+	router.HandleFunc("/user", controller.RegisterUserHandler(database))
+	router.HandleFunc("/products", controller.ProductsHandler(database))
+	handler := middleware.CORS(router)
 
 	// 8000番ポートでリクエストを待ち受ける
 	log.Println("Listening...")
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	if err := http.ListenAndServe(":8000", handler); err != nil {
 		log.Fatal(err)
 	}
-}
-
-// ③ Ctrl+CでHTTPサーバー停止時にDBをクローズする
-func closeDBWithSysCall() {
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
-	go func() {
-		s := <-sig
-		log.Printf("received syscall, %v", s)
-
-		if err := db.Close(); err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("success: db.Close()")
-		os.Exit(0)
-	}()
 }
