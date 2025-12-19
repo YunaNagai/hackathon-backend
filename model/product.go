b3f34498404588b74a6b6c9d2f53559b83aa11ad
YunaNagai <purple2yn@icloud.com>
2025-12-19 13:13:01 +0900

    update back

--- model/product.go
+++ model/product.go
@@ -1,14 +1,12 @@
 package model
 
-import "database/sql"
-
 type Product struct {
 	ID          string
-	SellerID    sql.NullString
-	Title       sql.NullString
-	Price       sql.NullInt64
-	Description sql.NullString
+	SellerID    string
+	Title       string
+	Price       int64
+	Description string
 	Status      string
-	CreatedAt   sql.NullTime
-	ImageURL    sql.NullString
+	CreatedAt   string
+	ImageURL    string
 }
