71b7e94f637dd7372f64453f394835e6dc7d0478
YunaNagai <purple2yn@icloud.com>
2025-12-19 18:17:43 +0900

    update back

--- usecase/transaction_usecase.go
+++ usecase/transaction_usecase.go
@@ -9,6 +9,3 @@ import (
 func GetTransactions(db *sql.DB) ([]model.Transaction, error) {
 	return dao.GetAllTransactions(db)
 }
-func GetTransactionByID(db *sql.DB, id string) (model.Transaction, error) {
-	return dao.GetTransactionByID(db, id)
-}
