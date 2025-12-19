e57947f66340ac3784f9ffb5756247ece2ed72bd
YunaNagai <purple2yn@icloud.com>
2025-12-19 17:42:45 +0900

    update back

--- model/product.go
+++ model/product.go
@@ -1,12 +1,12 @@
 package model
 
 type Product struct {
-	ID          string
-	SellerID    string
-	Title       string
-	Price       int64
-	Description string
-	Status      string
-	CreatedAt   string
-	ImageURL    string
+	ID          string `json:"id"`
+	SellerID    string `json:"sellerId"`
+	Title       string `json:"title"`
+	Price       int64  `json:"price"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+	CreatedAt   string `json:"createdAt"`
+	ImageURL    string `json:"imageUrl"`
 }
